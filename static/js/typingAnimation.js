884e8d7b1f362e3a61d7f7c3f24d8a8f014b189f
unknown <widiafandi58@gmail.com>
2022-07-27 17:14:14 +0700

    update: javascript function

--- static/js/typingAnimation.js
+++ static/js/typingAnimation.js
@@ -1,24 +1,3 @@
-const attr = ['bg-white', 'text-blue-1', 'shadow-lg']
-// remove attribut
-function removeAttr(attr) {
-  document.getElementById('navbar').classList.remove(attr);
-}
-// add attribut
-function addAttr(attr) {
-  document.getElementById('navbar').classList.add(attr);
-}
-// toggle attribut
-function toggleAttr(attr) {
-  document.getElementById('navbar').classList.toggle(attr);
-}
-// btn menus action
-function btnChangeColor() {
-  attr.forEach(removeAttr);
-  removeAttr('text-white');
-  toggleAttr('rounded-b-[20px]');
-  attr.forEach(toggleAttr);
-}
-
 /* type animation */
 const carouselText = [
   {text: "Widi Afandi"},
